65be4886fac4f691e5f458086251c37595b0beb2
Roy Chuang <roy_chuang@accton.com>
2016-10-05 16:13:31 +0800

    Add history

--- devicetypes/smc/smc-door-window-sensor.src/smc-door-window-sensor.groovy
+++ devicetypes/smc/smc-door-window-sensor.src/smc-door-window-sensor.groovy
@@ -12,6 +12,8 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  HISTORY:
+ *      2016/10/5    -- Roy Chuang, Create
  */
 
 metadata {
